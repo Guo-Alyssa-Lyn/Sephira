6763352314e6878e43ab1d199277edd7160a7c59
Lance Valle <vallelance8@gmail.com>
2025-03-10 20:05:05 +0800

    fix: remove unused database config and fix ESLint issues

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', 'nemesis.com'],
+    domains: ['nemesis.com'],
     remotePatterns: [
       {
         protocol: 'https',
